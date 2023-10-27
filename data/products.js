4b60bff7aa50b6d7f4900d6f0399b45277c459c6
vuvantai14 <147785839+vuvantai14@users.noreply.github.com>
2023-10-27 09:57:00 +0700

    Add files via upload

---
+++ data/products.js
@@ -0,0 +1,119 @@
+var list_products = [
+    {
+    "name": "Apple MacBook Air M1 256GB 2020",
+    "company": "Apple",
+    "img": "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/a/i/air_m2.png",
+    "price": "22.990.000",
+    "star": 4,
+    "rateCount": 10,
+    "promo": {
+        "name": "giareonline",
+        "value": "19.390.000"
+    },
+    "detail": {
+        "screen": "OLED, 13.3', Super Retina",
+        "os": "macOS Big Sur",
+        //"camara": "2 camera 12 MP",
+        "camaraFront": "GPU 7 nhân, 16 nhân Neural Engine",
+        "cpu": "8 nhân với 4 nhân hiệu năng cao và 4 nhân tiết kiệm điện",
+        "ram": "8 GB",
+        "rom": "256 GB SSD",
+        //"microUSB": "Không",
+        "battery": "49.9-watt lithium-polymer, củ sạc công suất 30W"
+    },
+    "masp": "App0"
+},{
+    "name": "Apple Macbook Air M2 2022 8GB 256GB",
+    "company": "Apple",
+    "img": "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/m/a/macbook_air_m22.png",
+    "price": "32.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "26.590.000"
+    },
+    "detail": {
+        "screen": "OLED, 13.6', Retina HD",
+        "os": "MacOS",
+        //"camara": "2 camera 12 MP",
+        //
+        "cpu": "Apple M2",
+        "camaraFront": "8 nhân GPU, 16 nhân Neural Engine",
+        "ram": "8 GB",
+        "rom": "256 GB",
+        //"microUSB": "Không",
+        "battery": "52,6 Wh"
+    },
+    "masp": "App1"
+},{
+    "name": "Apple Macbook Pro 13 M2 2022 8GB 256GB",
+    "company": "Apple",
+    "img": "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/p/r/pro-m2.jpg",
+    "price": "35.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "29.490.000"
+    },
+    "detail": {
+        "screen": "OLED, 13', 16 triệu màu",
+        "os": "MacOS",
+        //"camara": "12 MP",
+        //
+        "cpu": "Apple M2 tám nhân CPU",
+        "camaraFront": "10 nhân GPU",
+        "ram": "8 GB",
+        "rom": "256 GB SSD",
+        //"microUSB": "Không",
+        "battery": "58.2Whrs"
+    },
+    "masp": "App2"
+}, {
+    "name": "Asus VivoBook 15 OLED",
+    "company": "Asus",
+    "img": "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/t/e/text_ng_n_1__1_120.png",
+    "price": "20.990.000",
+    "star": 4,
+    "rateCount": 5,
+    "promo": {
+        "name": "giareonline",
+        "value": "17.990.000"
+    },
+    "detail": {
+        "screen": "OLED, 15.6, 16 triệu màu",
+        "os": "Windows 11 Home",
+        //"camara": "12 MP",
+        //
+        "cpu": "Intel Core i5-13500H 2.6 GHz (18MB Cache, up to 4.7 GHz, 12 lõi / 16 luồng)",
+        "camaraFront": "Intel Iris Xe Graphics",
+        "ram": "16 GB",
+        "rom": "512GB M.2 NVMe PCIe 3.0 SSD",
+        "battery": "50WHrs, 3S1P, 3-cell Li-ion"
+    },
+    "masp": "Asu0"
+}, {
+    "name": "Asus VivoBook Go 14",
+    "company": "Asus",
+    "img": "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/t/e/text_ng_n_-_2023-06-08t005130.908.png",
+    "price": "14.490.000",
+    "star": 4,
+    "rateCount": 804,
+    "promo": {
+        "name": "giareonline",
+        "value": "13.290.000"
+    },
+    "detail": {
+        "screen": "OLED, 14', Full HD+",
+        "os": "Windows 11 Home",
+        //"camara": "16 MP và 2 MP (2 camera)",
+        "cpu": "AMD Ryzen 5 7520U (4MB cache, up to 4.3 GHz, 4 lõi/8 luồng)",
+        "camaraFront": "AMD Radeon Graphics",
+        "ram": "16 GB",
+        "rom": "SSD 512GB M.2 NVMe PCIe 3.0",
+        //"microUSB": "MicroSD, hỗ trợ tối đa 128 GB",
+        "battery": "42WHrs, 3S1P, Li-ion 3 cell"
+    },
+    "masp": "Asu1"
+}]
