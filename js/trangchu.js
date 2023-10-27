fe9cd5dc3c763320f05e0a30c7d83a8aebc4c123
vuvantai14 <147785839+vuvantai14@users.noreply.github.com>
2023-10-27 10:12:37 +0700

    Update trangchu.js

--- js/trangchu.js
+++ js/trangchu.js
@@ -63,7 +63,7 @@ window.onload = function () {
 		//addKhungSanPham(' GIÁ SỐC ONLINE', yellow_red, ['star=3', 'sort=rateCount-decrease'], soLuong, div);
 		//addKhungSanPham('SẢN PHẨM MỚI', blue, ['promo=moiramat', 'sort=rateCount-decrease'], soLuong, div);
 		//addKhungSanPham('TRẢ GÓP 0%', yellow_red, ['promo=tragop', 'sort=rateCount-decrease'], soLuong, div);
-		addKhungSanPham('HOT SALE', yellow_red, ['promo=giareonline', 'sort=rateCount-decrease'], soLuong, div);
+		addKhungSanPham('LapTop', yellow_red, ['promo=giareonline', 'sort=rateCount-decrease'], soLuong, div);
 		//addKhungSanPham('GIẢM GIÁ LỚN', yellow_red, ['promo=giamgia'], soLuong, div);
 		//addKhungSanPham('GIÁ RẺ CHO MỌI NHÀ', green, ['price=0-3000000', 'sort=price'], soLuong, div);
 	}
